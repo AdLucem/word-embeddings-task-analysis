edb70679d661d74fa8fd01c6cfaeea86fe1c8365
Ghosal <ghosalatreyee@gmail.com>
2020-01-26 18:44:11 +0530

    need to run this on a GPU

--- embedding_module/run_system.py
+++
@@ -1,6 +0,0 @@
-"""Run the given embedding system on the given corpus
-and writing the resulting word:embedding pairs to file"""
-
-from elmo import run
-
-
--- requirements.sh
+++ requirements.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-# install allennlp
-pip install --user allennlp
-
-# install chainer, which for some reason
-# is a required module that context2vec
-# does not install
-pip install --user chainer
-
-# install cupy too, for the same reasons
-pip install --user cupy
-
-# install nltk for the corpuses
-pip install --user nltk
-
-# install plotly for the graphs
-pip install --user plotly
-
-# install context2vec
-cd $HOME
-git clone https://github.com/orenmel/context2vec.git
-cd context2vec
-python setup.py install
-
+tensorflow==1.13.1
+bert-serving-server
+bert-serving-client
+pytorch-pretrained-bert
+transformers
